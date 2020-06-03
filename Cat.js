6f7a3d319691455394ed91d5a56b2331decb0d10
Ngoc Van <vungoc.van.574@gmail.com>
2020-06-03 20:22:22 +0700

    mod eat for cat

--- Cat.js
+++ Cat.js
@@ -3,8 +3,8 @@ function Cat() {
     this.isDead = false;
 }
 
-Cat.prototype.eat(function(mouse) {
+Cat.prototype.eat = function(mouse) {
     this.stomach.push(mouse);
     this.isDead = true;
-})
+}
 module.exports = Cat;
